79a0742d9b90911b9d080c9555f6447d628732af
Carma <elfzdolinki@gmail.com>
2025-01-05 18:36:03 +0100

    YAY działa

--- Assets/kot.cs
+++ Assets/kot.cs
@@ -9,71 +9,99 @@ public class Kot : MonoBehaviour
     private float _timeSittingAround;
 
     [SerializeField] private float _launchPower = 500;
+    [SerializeField] private Kot[] _birds; // Tablica ptaków
+    private int _currentBirdIndex = 0; // Aktualnie u¿ywany ptak
     private Rigidbody2D _rigidbody2D;
     private SpriteRenderer _spriteRenderer;
 
     private void Awake()
     {
-        _initialPosition = transform.position;
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
+        // Inicjalizujemy wszystkie ptaki i dezaktywujemy je, poza pierwszym
+        for (int i = 0; i < _birds.Length; i++)
+        {
+            _birds[i].gameObject.SetActive(i == 0); // Aktywny tylko pierwszy ptak
+            _birds[i]._initialPosition = _birds[i].transform.position; // Zapisz ich pozycjê pocz¹tkow¹
+            _birds[i]._rigidbody2D = _birds[i].GetComponent<Rigidbody2D>();
+            _birds[i]._spriteRenderer = _birds[i].GetComponent<SpriteRenderer>();
+        }
     }
 
     private void Update()
     {
-        GetComponent<LineRenderer>().SetPosition(0, _initialPosition);
-        GetComponent<LineRenderer>().SetPosition(1, transform.position);
+        if (_currentBirdIndex >= _birds.Length) return;
 
-        if (_kotWasLaunched && _rigidbody2D.velocity.magnitude <= 0.1f)
+        var currentBird = _birds[_currentBirdIndex];
+
+        currentBird.GetComponent<LineRenderer>().SetPosition(0, currentBird._initialPosition);
+        currentBird.GetComponent<LineRenderer>().SetPosition(1, currentBird.transform.position);
+
+        if (currentBird._kotWasLaunched && currentBird._rigidbody2D.velocity.magnitude <= 0.1f)
         {
-            _timeSittingAround += Time.deltaTime;
+            currentBird._timeSittingAround += Time.deltaTime;
         }
 
-        if (transform.position.y > 6 || 
-            transform.position.y < -6 || 
-            transform.position.x > 20 ||
-            transform.position.x < -5 || 
-            _timeSittingAround > 2)
+        if (currentBird.transform.position.y > 6
+            || currentBird.transform.position.y < -6
+            || currentBird.transform.position.x > 20
+            || currentBird.transform.position.x < -5
+            || currentBird._timeSittingAround > 2)
         {
-            string currentSceneName = SceneManager.GetActiveScene().name;
-            SceneManager.LoadScene(currentSceneName);
+            NextBirdOrRestartLevel();
         }
     }
+
     private void OnMouseDown()
     {
-        // Only allow interaction if the bird hasn't been launched yet
-        if (_kotWasLaunched) return;
-        GetComponent<LineRenderer>().enabled = true;
+        if (_kotWasLaunched || _currentBirdIndex >= _birds.Length) return;
 
-        _spriteRenderer.color = Color.red;
+        var currentBird = _birds[_currentBirdIndex];
 
-        // Calculate the offset between the bird's position and the mouse position
+        currentBird.GetComponent<LineRenderer>().enabled = true;
+        currentBird._spriteRenderer.color = Color.red;
+
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        _dragOffset = transform.position - new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+        currentBird._dragOffset = currentBird.transform.position - new Vector3(mousePosition.x, mousePosition.y, currentBird.transform.position.z);
     }
 
     private void OnMouseUp()
     {
-        // Only allow launching if the bird hasn't been launched yet
-        if (_kotWasLaunched) return;
+        if (_kotWasLaunched || _currentBirdIndex >= _birds.Length) return;
 
-        _spriteRenderer.color = Color.white;
+        var currentBird = _birds[_currentBirdIndex];
 
-        Vector2 directionToInitialPosition = _initialPosition - transform.position;
-        _rigidbody2D.AddForce(directionToInitialPosition * _launchPower);
-        _rigidbody2D.gravityScale = 1;
-        _kotWasLaunched = true; // Set this to true to prevent future launches
+        currentBird._spriteRenderer.color = Color.white;
+        Vector2 directionToInitialPosition = currentBird._initialPosition - currentBird.transform.position;
+        currentBird._rigidbody2D.AddForce(directionToInitialPosition * _launchPower);
+        currentBird._rigidbody2D.gravityScale = 1;
+        currentBird._kotWasLaunched = true;
 
-         GetComponent<LineRenderer>().enabled = false;
+        currentBird.GetComponent<LineRenderer>().enabled = false;
     }
 
     private void OnMouseDrag()
     {
-        // Only allow dragging if the bird hasn't been launched yet
-        if (_kotWasLaunched) return;
+        if (_kotWasLaunched || _currentBirdIndex >= _birds.Length) return;
 
-        // Maintain the offset during dragging to prevent teleporting
+        var currentBird = _birds[_currentBirdIndex];
+
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z) + _dragOffset;
+        currentBird.transform.position = new Vector3(mousePosition.x, mousePosition.y, currentBird.transform.position.z) + currentBird._dragOffset;
+    }
+
+    private void NextBirdOrRestartLevel()
+    {
+        _birds[_currentBirdIndex].gameObject.SetActive(false); // Dezaktywuj obecnego ptaka
+        _currentBirdIndex++;
+
+        if (_currentBirdIndex < _birds.Length)
+        {
+            _birds[_currentBirdIndex].gameObject.SetActive(true); // Aktywuj kolejnego ptaka
+        }
+        else
+        {
+            // Jeœli wszystkie ptaki zosta³y u¿yte, restartujemy poziom
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
     }
 }
