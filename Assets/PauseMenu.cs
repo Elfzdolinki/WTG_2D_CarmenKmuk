c206078df2e8760427005ee2b8ed5659b36049ef
Carma <elfzdolinki@gmail.com>
2025-01-05 17:18:15 +0100

    pause menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void Restart()
+    {
+         SceneManager.LoadScene(SceneManager.GetActiveScene() .buildIndex);
+    }
+}
